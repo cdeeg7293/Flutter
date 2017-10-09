6a1b95eae350a0c060b25cc23a04940b85e15d0c
Conor Deegan <cdeeg1993@gmail.com>
2017-10-09 22:42:00 +0000

    Added Pages controller and views for Home, index, profile and explorer

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+#Pages controller controls the code for any page inside /pages
+class PagesController < ApplicationController
+  #back end code for pages/index
+  def index
+  end
+#back end code for pages/home
+  def home
+  end
+#back end code for pages/profile
+  def profile
+  end
+#back end code for pages/explorer
+  def explorer
+  end
+end
